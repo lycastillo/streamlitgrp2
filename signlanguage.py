e2325e7dd07d14379e232a287b58fba63a728334
rdiocera2002 <tokitaryuki12@gmail.com>
2024-05-15 12:38:34 +0800

    Update signlanguage.py

--- signlanguage.py
+++ signlanguage.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import tensorflow as tf
-import PIL
 from PIL import Image, ImageOps
 import numpy as np
 
@@ -18,11 +17,15 @@ st.write("""
 file = st.file_uploader("Choose a hand gesture from the photos", type=["jpg", "png"])
 
 def import_and_predict(image_data, model):
-    size = (64, 64, 3)  # Match the input size with the Google Colab code
+    size = (64, 64)  # Match the input size with the Google Colab code
     image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
     img = np.asarray(image)
     img = img / 255.0  # Normalize pixel values
     img = np.expand_dims(img, axis=0)
+
+    # Reshape input according to the model's input shape
+    img_reshape = tf.image.resize(img, [64, 64])  # Resize to (64, 64)
+    
     prediction = model.predict(img_reshape)
     return prediction
 
