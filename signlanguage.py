fc99112f4091d5493f52c9061fdaebc587ca6e03
rdiocera2002 <tokitaryuki12@gmail.com>
2024-05-15 11:38:10 +0800

    Update signlanguage.py

--- signlanguage.py
+++ signlanguage.py
@@ -8,22 +8,23 @@ import numpy as np
 def load_model():
   model=tf.keras.models.load_model('signlanguage.h5')
   return model
+
 model=load_model()
+
 st.write("""
 # American Sign Language Identifier"""
 )
 file=st.file_uploader("Choose a hand gesture from the photos",type=["jpg","png"])
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (150, 150)  # Match the input size with the Google Colab code
+    image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
+    img = np.asarray(image)
+    img = img / 255.0  # Normalize pixel values
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img_reshape)
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
