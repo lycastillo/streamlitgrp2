2a90c49e95bd0e2748061091d14816bd36e8328c
rdiocera2002 <tokitaryuki12@gmail.com>
2024-05-15 11:46:05 +0800

    Update signlanguage.py

--- signlanguage.py
+++ signlanguage.py
@@ -1,39 +1,40 @@
 import streamlit as st
 import tensorflow as tf
 import cv2
-from PIL import Image,ImageOps
+from PIL import Image, ImageOps
 import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('signlanguage.h5')
-  return model
+    model = tf.keras.models.load_model('signlanguage.h5')
+    return model
 
-model=load_model()
+model = load_model()
 
 st.write("""
-# American Sign Language Identifier"""
-)
-file=st.file_uploader("Choose a hand gesture from the photos",type=["jpg","png"])
+# American Sign Language Identifier
+""")
+
+file = st.file_uploader("Choose a hand gesture from the photos", type=["jpg", "png"])
 
 def import_and_predict(image_data, model):
     size = (150, 150)  # Match the input size with the Google Colab code
-    image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use Image.LANCZOS for resizing
     img = np.asarray(image)
     img = img / 255.0  # Normalize pixel values
     img = np.expand_dims(img, axis=0)
-    prediction = model.predict(img_reshape)
+    prediction = model.predict(img)
     return prediction
 
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['0', '1', '2', '3', '4', '5', '6', '7','8',
-                  '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-                  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
+                   '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                   'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    string = "OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
